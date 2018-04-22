eb4b023d2d171db84c0db6b4434140a885c1c2bf
Tada0 <tomekholda@gmail.com>
2018-04-22 21:18:56 +0200

    code update due to toddlernetwork module change

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import digits_recognition as DR
-import ftplib
-import pickle
 from toddlernetwork import neural_network as NN
 
 if __name__ == '__main__':
 
-    n = NN.NeuralNetwork(784, 50, 10)
-    n.set_learning_rate(0.003)
-    DR.Train_Network(n, 1000)
+    n = NN.NeuralNetwork.load('2018_04_22___21_15_40___784_50_10.tnn')
     DR.Test_Network(n)
-    n.save()
 
-    session = ftplib.FTP('keikeikei.ugu.pl', 'keikeikei.ugu.pl', 'keikeikei123')
-    file = open("nn.tnn", 'rb')
-    session.storbinary('STOR nn.tnn', file)
-    file.close()
-    session.quit()
